c4cf9edc147fe86ffef120fc065a0b8fb43b6724
AhmadKabakibi <ahmadkbakibi@gmail.com>
2018-08-21 21:14:37 +0200

    add initial setup for the iparkmini and init config

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ switch (NODE_ENV) {
     configBuffer = fs.readFileSync(path.resolve(__dirname, 'production.json'), 'utf-8')
     break
   case 'development':
-    configBuffer = fs.readFileSync(path.resolve(__dirname, 'staging.json'), 'utf-8')
+    configBuffer = fs.readFileSync(path.resolve(__dirname, 'development.json'), 'utf-8')
     break
   default:
     configBuffer = fs.readFileSync(path.resolve(__dirname, 'default.json'), 'utf-8')
