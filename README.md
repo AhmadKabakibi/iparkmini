137984c79126b045fb61ec8f35965a0f57469313
AhmadKabakibi <ahmadkbakibi@gmail.com>
2018-08-21 21:12:02 +0200

    add initial setup for the iparkmini

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # iparkmini
 :parking: iparkmini :blue_car: 
+
+
+[![js-standard-style](https://cdn.rawgit.com/feross/standard/master/badge.svg)](https://github.com/feross/standard)
+
+[![js-standard-style](https://img.shields.io/badge/code%20style-standard-brightgreen.svg?style=flat)](https://github.com/feross/standard)
+
---
+++ index.js
@@ -0,0 +1,20 @@
+if (!process.env.NODE_ENV) { require('dotenv').config() }
+require('./lib/utils/check-env').check()
+
+const log = require('./lib/utils/logger')
+const app = require('express')()
+require('./lib/controllers')(app)
+
+try {
+  const server = require('http').createServer(app)
+  require('./lib/utils/web-sockets').registerServer(server)
+  server.listen(process.env.PORT)
+  log.info(`API booted`)
+  log.info(`Environment          : ${process.env.NODE_ENV}`)
+  log.info(`Port                 : ${server.address().port}`)
+} catch (error) {
+  log.error(`Error starting API`)
+  log.error(error)
+}
+
+module.exports = app
